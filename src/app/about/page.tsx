ca2f342d8acb3a8284217ec122203605220fe9c0
Jakub Przyciasa <jakubprzyciasa@gmail.com>
2026-01-04 20:36:03 +0100

    feat: add about me content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,16 +13,7 @@ function page() {
       <Header isScrolled={true} />
       <h2 className="font-volkhov py-5 text-2xl md:pl-40 pl-5">About</h2>
       <p className="font-volkhov py-5 text-sm md:px-40 px-5 2xl:w-1/2 xl:w-3/5 md:w-4/5 w-full">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit
-        nesciunt inventore possimus eius nobis odio voluptas ea maxime adipisci
-        quo sed id iste perferendis non, beatae iusto hic sint amet? Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Tempora, reprehenderit
-        perferendis. Quisquam, repellendus a. Quia vel, labore, ducimus
-        perferendis ratione placeat quod possimus consequuntur rerum tempore,
-        natus illum delectus accusamus. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Assumenda neque molestias placeat ipsum temporibus
-        tempore commodi. Porro, aliquid beatae. Ut ad numquam voluptas culpa
-        doloribus, corporis consequatur eius deleniti et.
+        Hi, I'm Jacob and I am photographer from Poland. Since the age of 15, I have been creating photos that reflect my style and help others in their various projects. I have completed a professional portrait photography course and have been awarded in local and international photography competitions. In my photography journey I had a pleasure to work with firms like Autodrom Sosnowiec, Wynajmij Studio and FD Studio. If you need the highest quality photos for an event, project or just for fun, feel free to contact me. Together, we will create amazing things!
       </p>
       <div className="relative w-full h-120 flex justify-center items-center mt-20">
         <Image
